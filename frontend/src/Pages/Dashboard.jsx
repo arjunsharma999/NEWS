c68209617d6764876e76ecc48b5df7e143f3f41e
arjun sharma <arjunsharmarke99@gmail.com>
2023-07-28 23:14:28 +0530

    resolve

--- frontend/src/Pages/Dashboard.jsx
+++ frontend/src/Pages/Dashboard.jsx
@@ -4,18 +4,7 @@ import {useEffect,useState} from 'react';
  
 const Dashboard = () => {
    
-   const[record,setRecord] = useState([])
- 
-   const getData = () =>
-   {
-       fetch('https://jsonplaceholder.typicode.com/users')
-       .then(resposne=> resposne.json())
-       .then(res=>setRecord(res))
-   }
- 
-   useEffect(() => {
-      getData();
-   },)
+   
     
  
     return (
